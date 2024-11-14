bd43838b240acbee5727f8f6ac14046e180c9651
Dani Milagros Astonitas Vallejos <milagrosastonitasvallejos@gmail.com>
2024-11-14 12:38:00 -0500

    Creacion de clases

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Mi nimbre es Milagros");
-Console.WriteLine("Mia apellidos son:Astonitas Vallejos");
+Console.WriteLine("Mi nombre es Milagros");
+
